dec226b6115dcb10ff60fa510433aacb2ee0b4ff
Adrian <adriantesta00@gmail.com>
2021-04-10 09:49:10 -0400

    Programa terminado

--- cajerito.pas
+++ cajerito.pas
@@ -1,5 +1,6 @@
 program cajero_automata;
 
+
 uses crt;
 
 type
@@ -11,6 +12,8 @@ type
 		saldo: integer;
 		contasena: integer;
 		movimientos: integer;
+		movimientos_Dato: array[1..50] of string;
+		movimientos_valor: array[1..50] of integer;
 	end;
 
 
@@ -19,9 +22,31 @@ type
 var
 
 	base_datos: array[1..10] of registro;
-	interfaz,nuevo_usa: boolean;
+	interfaz,nuevo_usa,lala,lili: boolean;
 	opci,oti: string;
-	i,nuevo,new_usa,valor_ingresado,control,cedula_deposi,l: integer;
+	i,nuevo,new_usa,valor_ingresado,control,cedula_deposi,l,R: integer;
+	numer: integer;
+	archivo: text;
+	linea, imprimir: string; 
+
+
+function FileExists(nombre: String): boolean;
+var
+    f: text;
+begin
+    assign(f,nombre);
+    {$I-}
+    reset(f);
+    {$I+}
+    if (IOResult = 0) then
+    begin
+        close(f);
+        FileExists:=true;
+    end
+    else
+        FileExists:=false;
+end;
+
 begin
 	nuevo:= 1;
 	interfaz:= false;
@@ -152,6 +177,19 @@ begin
 
 											end;
 											base_datos[i].saldo:= base_datos[i].saldo - valor_ingresado;
+											base_datos[i].movimientos:= base_datos[i].movimientos + 1;
+											
+											l:= 1; 
+											lala:= false;		
+											Repeat
+												if base_datos[i].movimientos_valor[l] = 0 then
+												begin 
+													base_datos[i].movimientos_Dato[l]:= ' Se ha Retirado ';
+													base_datos[i].movimientos_valor[l]:= base_datos[i].movimientos_valor[l] + valor_ingresado;
+													lala:= true;
+												end;
+												l:= l + 1 ;
+											until(lala);
 										until(control = 1);
 
 									end;
@@ -184,6 +222,20 @@ begin
 											Readln(control);
 										until(control = 1);
 										base_datos[i].saldo:= base_datos[i].saldo + valor_ingresado;
+										base_datos[i].movimientos:= base_datos[i].movimientos + 1;
+										
+										l:= 1; 
+										lala:= false;		
+										Repeat
+											if base_datos[i].movimientos_valor[l] = 0 then
+											begin 
+												base_datos[i].movimientos_Dato[l]:= ' Se ha Depositado ';
+												base_datos[i].movimientos_valor[l]:= base_datos[i].movimientos_valor[l] + valor_ingresado;
+												lala:= true;
+											end;
+											l:= l + 1 ;
+										until(lala);
+
 
 									end;
 								end;
@@ -215,6 +267,21 @@ begin
 											Readln(control);
 										until(control = 1);
 										base_datos[i].saldo:= base_datos[i].saldo - valor_ingresado;
+										base_datos[i].movimientos:= base_datos[i].movimientos + 1;
+
+										l:= 1; 
+										lala:= false;		
+										Repeat
+											if base_datos[i].movimientos_valor[l] = 0 then
+											begin 
+												
+												base_datos[i].movimientos_Dato[l]:= ' Se ha Trasferido ';
+												base_datos[i].movimientos_valor[l]:= base_datos[l].movimientos_valor[l] + valor_ingresado;
+												lala:= true;
+											end;
+											l:= l + 1 ;
+										until(lala);
+
 										Clrscr;
 										writeln('Dame el numero de la cedula del usuario a Trasferir');
 										Readln(cedula_deposi);
@@ -224,7 +291,6 @@ begin
 											begin 
 
 												base_datos[l].saldo:= base_datos[l].saldo + valor_ingresado;
-
 
 											end;
 
@@ -246,8 +312,94 @@ begin
 				end;
 
 			'4':begin
-				Clrscr;
-				writeln('Has selecionado la Hitorial');
+					Clrscr;
+					writeln('Has selecionado la Hitorial');
+					writeln('Dame tu cedula');
+					Readln(valor_ingresado);
+					For i:= 1 to 10 do
+					begin
+						if base_datos[i].cedula = valor_ingresado  then
+						begin
+							writeln('Dame contrasena');
+							Readln(valor_ingresado);
+							if base_datos[i].contasena = valor_ingresado  then
+							begin
+								Clrscr;
+								writeln('Tus movimientos Bancarios son: ',base_datos[i].movimientos);
+								writeln('');
+									l:= 1; 
+									lala:= false;		
+									Repeat
+										if base_datos[i].movimientos_valor[l] <> 0 then
+										begin 
+											
+
+											writeln(base_datos[i].movimientos_Dato[l], base_datos[i].movimientos_valor[l] );
+
+										end else
+										begin 
+
+											lala:= true;
+											Repeat
+												writeln('¿Desea imprimir una factura de los movimientos Bancarios y Generarlos en un archivo TXT?');
+												writeln('(1) SI (0) NO ');
+												Readln(control);
+												if control = 1 then
+												begin 
+
+													assign(archivo, 'Factura_usuario.txt');
+
+													if FileExists( 'Factura_usuario.txt' ) then
+													begin
+														writeLn('El fichero Factura_usuario.txt existe.');
+														writeLn('Ya puedes Retirarla');
+														Append(archivo);
+													end else
+													begin
+														writeLn('El fichero Factura_usuario.txt no existe! creando...');
+
+														writeLn('Ya puedes Retirarla');
+
+														rewrite(archivo); 
+													end;
+														
+
+													writeln(archivo,'Tus movimientos de datos son: ');
+													writeln(archivo,' ');
+													writeln(archivo,base_datos[i].movimientos);
+													writeln(archivo,' ');
+													R:=1;
+													Repeat
+
+														writeln(archivo,base_datos[i].movimientos_Dato[R]);
+														writeLn(archivo, ' ');
+														writeLn(archivo,base_datos[i].movimientos_valor[R]);
+														R:= R + 1;
+													until (base_datos[i].movimientos_valor[R] = 0);
+													close(archivo);
+
+													lili:= true;
+
+
+												end else
+												begin 
+
+
+													lili:= true;
+
+												end;
+											until(lili);
+										
+
+										end;
+
+										l:= l + 1 ;
+									until(lala);
+									Readln();
+							end;
+						end;
+					end;
+
 				end;
 
 			'5':begin
